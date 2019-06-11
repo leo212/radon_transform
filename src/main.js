069451386b01a3eb7ba9e21c9b3339cff13ba35d
Lior Tamam <leo212@gmail.com>
2019-06-11 23:19:46 +0300

    added a similarity score on reconstruct page.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
 import App from "./App.vue";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 import "vuetify/dist/vuetify.min.css";
-
+import "./plugins/vuetify";
 import VueMaterial from "vue-material";
 
-// Vue.use(BootstrapVue);
 Vue.use(VueMaterial);
-Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
 // noinspection JSUnusedGlobalSymbols
 new Vue({
     render: h => h(App)
